9cc75e9f52fed577870c5629731605249f4bb834
rssuryakkl <148074087+rssuryakkl@users.noreply.github.com>
2023-11-05 21:45:19 +0530

    Add files via upload

---
+++ DAY2.py
@@ -0,0 +1,23 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hii  This is day 2 "),print ("we discuss about  basic addition in our day 2 class")
+Hii  This is day 2 
+we discuss about  basic addition in our day 2 class
+(None, None)
+>>> a=10
+>>> b=50
+>>> a+b
+60
+>>> c=a+b
+>>> c
+60
+>>> print(c)
+60
+>>> print("The Sum of :",c)
+The Sum of : 60
+>>> print("The Sum of ",a,"and",b,"is:"c)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print("The Sum of ",a,"and",b,"is:",c)
+The Sum of  10 and 50 is: 60
+>>> print("thank you all until this is rs bye all")
+thank you all until this is rs bye all
