9eb02f24be02a8622c474af90ef965d54b4bc456
rssuryakkl <148074087+rssuryakkl@users.noreply.github.com>
2023-11-05 21:41:07 +0530

    Add files via upload

---
+++ DAY6.py
@@ -0,0 +1,36 @@
+# "#" -->represent a single line comment
+#Hello all this day6  today we are going to seee a topic on  Literals
+# literals are in three types 1) Numeric 2) String 3) Boolean
+
+#1) Numeric
+a = 0b1010 #Binary Literals
+b = 100 #Decimal Literal
+c = 0o310 #Octal Literal
+d = 0x12c #Hexadecimal Literal
+print ("Integer Literals :",a,b,c,d)
+#Float Literal
+float_1 = 10.5
+float_2 = 1.5e2
+print ("Float Literals :",float_1,float_2)
+#Complex Literal
+x = 1 + 3.14j
+print ("Complex Literals :")
+print ("x = ", x , "Imaginary part of x = ", x.imag, "Real part of x = ", x.real)
+
+#2) String
+strings = "This is Python"
+char = "C"
+multiline_str = '''This is a multiline string with more than one line code.'''
+print (strings)
+print (char)
+print (multiline_str)
+
+#3) Boolean Literals
+
+boolean_1 = False
+boolean_2=True
+print("The  boolean value ")
+print("Boolean value of 1:",boolean_1)
+print("Boolean value of 2:",boolean_2)
+
+#4.
