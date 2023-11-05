24771f4e3118cfd8d24eff18013b82753356a3e5
rssuryakkl <148074087+rssuryakkl@users.noreply.github.com>
2023-11-05 21:41:31 +0530

    Add files via upload

---
+++ DAY8.py
@@ -0,0 +1,26 @@
+#hello guys this is day 8 (05.11.2023) today we are going to see a topic on control statements
+#control statements has a three types
+#1.Sequential
+#2.Alternative or Branching
+#3.Iterative or Looping
+
+#1.Sequential
+print("1.Sequential")
+print("plot no:40,t-nagar,chennai")
+
+#2.Alternative or Branching
+#(i) Simple if statement
+
+x=int(input("Enter Your Age:"))
+if x>=18:
+    print("Your are  allowed,because your age is",x)
+    
+#(ii) if..else statement
+
+age=int(input("Enter your Age for eligiable or Not for voting:"))
+if (age>=18):
+    print("Your are eligible for voting")
+else:
+    print("Your are not eligible for voting")
+
+#that's all about today class we will continue tommrow until this is rs bye
