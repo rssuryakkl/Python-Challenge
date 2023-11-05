865fa3717e8dcee24da363b6fa610a63a38b552d
rssuryakkl <148074087+rssuryakkl@users.noreply.github.com>
2023-11-05 21:41:21 +0530

    Add files via upload

---
+++ DAY7.py
@@ -0,0 +1,40 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello Guys This is DAY 7 today we are going tio see a topic on Escape Sequences")
+Hello Guys This is DAY 7 today we are going tio see a topic on Escape Sequences
+print("\\test")
+\test
+print("\\Surya")
+\Surya
+#this is backslash
+# now we are going to single-quote
+print("Doesn\'t")
+Doesn't
+#now double-quotes
+printf("\"Hi This is RS"")
+       
+SyntaxError: incomplete input
+>>> printf("\"Hi This is RS\"")
+...        
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    printf("\"Hi This is RS\"")
+NameError: name 'printf' is not defined. Did you mean: 'print'?
+>>> print("\"Hi This is RS\"")
+...        
+"Hi This is RS"
+>>> #now new-line
+...        
+>>> print("hi","\n","RS")
+...        
+hi 
+ RS
+>>> #now tab space
+...        
+>>> print("Hello","\t","world!")
+...        
+Hello 	 world!
+>>> 
+>>> print("So thats all about today s class until this is RS bye date:30-10-2023")
+...        
+So thats all about today s class until this is RS bye date:30-10-2023
