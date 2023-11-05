1f95d389ee9d408149903c3808905146a4cc8a28
rssuryakkl <148074087+rssuryakkl@users.noreply.github.com>
2023-11-05 21:40:49 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,84 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hai All This is DAY5  today we going to see a topic  on   Relational or Comparative operators , Logical operators, Assignment operators & Conditional operator")
+Hai All This is DAY5  today we going to see a topic  on   Relational or Comparative operators , Logical operators, Assignment operators & Conditional operator
+print("1st topic is : Relational or Comparative operators")
+1st topic is : Relational or Comparative operators
+a=100
+b=35
+a==b
+False
+a>b
+True
+a<b
+False
+a>=b
+True
+a<=b
+False
+a!=b
+True
+
+print ("This about the relational operator")
+This about the relational operator
+
+print("2nd  topic is : Logical operators")
+2nd  topic is : Logical operators
+a=98
+b=34
+a==b or a>b
+True
+a==b and a>b
+False
+nota>b
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    nota>b
+NameError: name 'nota' is not defined
+not a>b
+False
+
+print ("This about the  Logical operators")
+This about the  Logical operators
+
+print("3rd  topic is : Assignment operators ")
+3rd  topic is : Assignment operators 
+x=10
+x+=20 # x=x+20
+x
+30
+x-=5 # x=x-5
+x
+25
+x*=5 # x=x*5
+x
+125
+ x/=2 # x=x/2
+ 
+SyntaxError: unexpected indent
+x/=2 # x=x/2
+>>> x
+62.5
+>>> x%=3 # x=x%3
+>>> x
+2.5
+>>> x**=2 # x=x**2
+>>> x
+6.25
+>>> x//=3
+>>> print("The",x)
+The 2.0
+>>> 
+>>> print("4th  topic is : Conditional operator")
+4th  topic is : Conditional operator
+>>> min= 49 if 49<50 else 50 # min = 49
+>>> 
+>>> a=30
+>>> b=50
+>>> min = a if a < b else b 
+>>> print ("The Minimum of A and B is ",min)
+The Minimum of A and B is  30
+>>> 
+>>> 
+>>> print("Thank you all until this is rs bye see u all in next day")
+Thank you all until this is rs bye see u all in next day
