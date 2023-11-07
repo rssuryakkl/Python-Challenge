c2684569ac0d31dec0771b3d890aefc8a16d68d1
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-07 18:21:01 +0530

    Add files via upload

---
+++ DAY10.py
@@ -0,0 +1,27 @@
+''' hello all this is DAY10(07-11-2023)today er aer going to see a
+topic on : Iteration or Looping constructs
+has a 2types:
+1.while loop
+2.for loop'''
+
+#while loop
+
+i=10
+while(i<=15):
+    print(i,end="\t")
+    i=i+1
+else:
+    print ("\nValue of i when the loop exit ",i)
+
+
+#print odd numbers using while loop
+num = 1
+while (num<=20):
+    print(num,end='\t')
+    num+=2
+
+#print even numbers using while loop
+num = 2
+while (num<10):
+    print(num,end='\t')
+    num+=2
