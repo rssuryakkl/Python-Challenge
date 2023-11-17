1535f85e2a0dffd8899071c1cc6140abf3e089dc
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-17 18:42:03 +0530

    Add files via upload

---
+++ DAY13.py
@@ -0,0 +1,33 @@
+#hi all this day13(17.11.2023) we are going to see a topic on Jump Statements in Python
+#(i) break statement
+for word in " Surya hii hello":
+    if word =='a':
+        break
+    print(word,end = '')
+
+
+#(ii) continue statement
+for hi in "this is daay one thraee":
+    if hi == 'a':
+        continue
+    print(hi,end='')
+
+
+#(iii) pass statement
+a = int (input("Enter a Number:"))
+if(a==0):
+    pass
+else:
+    print("Entered value is non Zero")
+
+
+
+
+
+
+
+
+
+
+
+
