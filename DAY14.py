35e993883f01012bf508dae485be2fe2afdfdc51
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-18 22:58:13 +0530

    Add files via upload

---
+++ DAY14.py
@@ -0,0 +1,20 @@
+#hii all this is DAY14 (18-11-2023)  pass statement and  example
+
+#pass statement 
+a = int(input("Enter:"))
+if a==0:
+    pass
+else:
+    print("Non zero")
+
+
+#Write a program to check whether the given character is a vowel or not.
+x=input("Enter a Letter: ")
+if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U'):
+    print("Vowel")
+else:
+    print("Consonant") 
+
+
+#thank you next  day we will see a topic on function and problems
+    #until this is Rs bye
