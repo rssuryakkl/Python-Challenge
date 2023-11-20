db389b6e73e3aca66424ad178c24dfddac7b5adf
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-20 20:49:28 +0530

    Add files via upload

---
+++ DAY16.py
@@ -0,0 +1,67 @@
+#hi all this is DAY16(20.11.2023) we are going to discuss about Scope of Variables
+# local Sccope
+'''
+def lo():
+    y=10
+    print(y)
+lo()
+
+
+
+
+
+
+def lo():
+    y=10
+lo()
+print(y)
+
+
+
+
+
+#global variable
+a=9
+def io():
+    print(a)
+io()
+
+c = 1
+def add():
+    a = c + 2
+    print(a)
+add()
+
+
+
+
+
+
+
+'''
+
+
+#Global and local variables
+
+c=9
+def add():
+    y="local"
+    a=c*2
+    print(a)
+    print(y)
+add()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
