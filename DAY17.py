d44018f4b894e73feab25befcdcaa87b4fa1902f
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-21 21:16:10 +0530

    Add files via upload

---
+++ DAY17.py
@@ -0,0 +1,58 @@
+#hii all this is DAY 17(21.11.2023) we are going to discuss about Global and local variables
+'''
+
+x=9
+def aoo():
+    y="hiii x print your value"
+    c=x*2
+    print(y)
+    print(c)
+aoo()
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+c=89
+def ado():
+    c=90
+    print("Local C:",c)
+ado()
+print("Global C:",c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
