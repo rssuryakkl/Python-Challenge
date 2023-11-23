314268f05a4d3275b10b964163732d10f2cd2518
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-23 22:16:57 +0530

    Add files via upload

---
+++ DAY19.py
@@ -0,0 +1,53 @@
+#Hi all this is DAY19(23.11.2023) today we are going to see a topic on Modifying and Deleting Strings
+
+str="how are You"
+print(str)
+str="how old are You"
+print(str)
+
+
+
+
+
+
+
+
+
+str="Hoe ar you"
+print(str.replace("e","w"))
+
+
+a = "Hello, World!"
+print(a.lower())  #lower mean captilized word's are changed into  small
+
+
+
+
+
+
+
+
+
+str11=input("Enter a String:")
+index=0
+for i in str11:
+    print("Substring[",index,"]:",i)
+    index+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
