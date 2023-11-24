7b2539a3f1ceb5b960d2d3d4aa4f59c2af742aa0
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-24 18:15:33 +0530

    Add files via upload

---
+++ DAY18.py
@@ -0,0 +1,7 @@
+#hi all This is DAY18(22-11-2023) we are going to discuss a topic on  Accessing characters in a String
+
+str1=input("Enter a String:")
+index=0
+for i in str1:
+    print("Subscript[",index,"]:",i)
+    index+= 1
