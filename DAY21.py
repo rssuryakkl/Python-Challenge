91d4f5efa9923e81cf577437cab59017e60c3230
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-25 18:44:40 +0530

    Add files via upload

---
+++ DAY21.py
@@ -0,0 +1,38 @@
+#hii all This is DAY21 (25-11-2023)  today we are going to see a topic on Stride when slicing string
+
+hii="HI ALL WELCOME TO LEARN PYTHON"
+print(hii[0:15])
+print(hii[0:30])#start,stop
+print(hii[0:15:2])#start,stop,step
+print(hii[::2])
+print(hii[::4])
+print(hii[0:6])
+print(hii[::-2])
+
+name="SURYA"
+Age=19
+print("Name:%s and Age:%d"%(name,Age))
+
+#Membership Operators
+a=input("Enter a String:")
+b="Chennai"
+if a in b:
+    print("Found")
+else:
+    print("Not Found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
