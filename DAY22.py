9614348b0f26cb3b5ae8a60210983a2b7eaf7626
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-26 21:24:06 +0530

    Add files via upload

---
+++ DAY22.py
@@ -0,0 +1,16 @@
+#hii all this is DAY22(26-11-2023) today we are  going to see a topic on Program to display the following pattern
+#Program to display the following pattern
+
+str='*'
+i=1
+while(i<=5):
+    print(str*i)
+    i+=1
+
+
+
+str='surya '
+i=1
+while(i<=5):
+    print(str*i)
+    i+=1
