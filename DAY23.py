349b3af61387ed3c18641b5c65e19ff0fda64d12
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-27 18:55:41 +0530

    Add files via upload

---
+++ DAY23.py
@@ -0,0 +1,36 @@
+#Hii all This is DAY23(27-11-2023) Today we are going to see a topic list
+#Create a List in Python
+
+my_list=[33,99,67,23,33]
+print(my_list)
+
+my_list=[33,99,67,23,33]
+my_list=[88,90,76]
+print(my_list)
+
+#Accessing List elements
+hiii=[12,34,56,77,88,99]
+print(hiii[2])
+
+hiii=[12,34,56,77,88,99]
+print(hiii[-1])
+
+#Accessing all elements of a list
+list=[9,8,7,4,5,23,3,2]
+i=0
+while i<=4:
+    print(list[i])
+    i=i+1
+
+#(ii) Reverse Indexing
+list=[9,8,7,4,5,23,3,2]
+i=-1
+while i>=-8:
+    print(list[i])
+    i=i-1
+
+
+
+
+    
+    
