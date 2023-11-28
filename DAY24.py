0cef17d45fbd47d4feae06de65e11bb1fff2d60e
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-28 18:45:58 +0530

    Add files via upload

---
+++ DAY24.py
@@ -0,0 +1,53 @@
+#hii all this is DAY24(28-11-2023)today's topic is List Length
+
+lists=["surya","welcome","To","learn","python"]
+print(len(lists))
+
+
+kk=["Tamil","English","Maths","Science","Social"]
+i=0
+while i<len(kk):
+    print(kk[i])
+    i=i+1
+
+
+tt=[45,676,897,324,565]
+for i in tt:
+    print(i)
+
+    
+#Python program to update/change single value
+mylist=[90,34,56,2,45]
+print("Element is no added")
+for i in mylist:
+    print(i)
+mylist[3]=23
+print("Element is updated")
+for y in mylist:
+    print(y)
+
+
+
+# Python program to update/change range of values
+hi = [2,3,5,7,9,89]
+print("Old List")
+for i in hi:
+    print(i)
+hi[0:6]=22,33,44,55,66,77
+print("Updated List")
+for x in hi:
+    print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
