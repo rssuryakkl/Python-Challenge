0c0b20ddc4ffa2f46cf9219c97c5f88cfa00a21a
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-29 19:25:06 +0530

    Add files via upload

---
+++ DAY25.py
@@ -0,0 +1,33 @@
+#hii all this is DAY25(29-11-2023) today we are going to discuss about  Adding more elements in a list
+#append
+listt = [23,45,7667,90]
+listt.append(91)
+print(listt)
+
+#extend
+listt.extend([92,93,94])
+print(listt)
+
+#example and insert
+MyList=[34,98,47,'Kannan', 'Gowrisankar', 'Lenin', 'Sreenivasan' ]
+print(MyList)
+MyList.insert(3,'Surya')
+print(MyList)
+
+# deleting elsment in list
+MySubjects = ['Tamil', 'Hindi', 'Telugu', 'Maths']
+print(MySubjects)
+del MySubjects[1]
+print(MySubjects)
+
+
+
+
+
+
+
+
+
+
+
+
