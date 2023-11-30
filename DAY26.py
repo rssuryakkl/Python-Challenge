b996856801bd2516fc1e1809127f4a5759a7cbc6
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-11-30 18:00:21 +0530

    Add files via upload

---
+++ DAY26.py
@@ -0,0 +1,37 @@
+#hii all This is DAY26 (30-11-2023) today we are going to discuss  about list delete and remove
+
+hiii = ['tamil','eng','mat','science','social']
+print(hiii)
+del hiii[1]
+print(hiii)
+
+hi1 = ['welcome','to','learn','python','hihello']
+print(hi1)
+hi1.remove('to')
+print(hi1)
+hi1.pop(1)
+print(hi1)
+
+#List and range ( ) function
+for i in range(1,11): #1 is start value,11 is stop value
+    print(i)
+
+for i in range(1,11,2): # #1 is start value,11 is stop value , 2is step value
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
