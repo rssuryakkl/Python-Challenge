bc51ae1f8165a0f66ff05e9b3113fe5e5cb1ef9f
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-12-03 20:40:10 +0530

    Add files via upload

---
+++ DAY29.py
@@ -0,0 +1,41 @@
+#hii All This is DAY29(03-12-2023) today's topic is Tuples
+
+MyTup1 = (23, 56, 89, 'A', 'E', 'I', "Tamil")
+print(MyTup1)
+MyTup1 = 23, 56, 89, 'A', 'E', 'I', "Tamil"
+print(MyTup1)
+print(type(MyTup1))
+
+#Creating tuples using tuple( ) function
+MyTup1 =tuple([ 23, 56, 89, 'A', 'E', 'I', "Tamil"])
+print(MyTup1)
+print(type(MyTup1))
+MyTup1 =([ 23, 56, 89, 'A', 'E', 'I', "Tamil"])
+print(MyTup1)
+print(type(MyTup1))
+
+# Creating Single element tuple
+hi=90
+print(hi)
+print(type(hi))
+hi=90,
+print(hi)
+print(type(hi))
+
+#thank you until this is RS bye
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
