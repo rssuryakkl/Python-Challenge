568c239cfb61d01a8e02129fd0e3e09bebcabf65
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-12-09 21:29:25 +0530

    Add files via upload

---
+++ DAY35.py
@@ -0,0 +1,9 @@
+#hii all This is DAY35(09-12-2023) problems
+#Write a program to display sum of natural numbers, upto n
+
+n=int(input("Enter a number: "))
+sum1 = 0
+while(n > 0):
+    sum1=sum1+n
+    n=n-1
+print("The sum of first n natural numbers is",sum1)
