ec7c2e9c0524f4d8720b336bcaf4aa5bf239fda4
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-12-21 20:32:34 +0530

    Add files via upload

---
+++ DAY47.py
@@ -0,0 +1,9 @@
+#hii all This is DAY47(21-12-2023) today's topic is Pattern
+
+row = int(input("Enter a Number:"))
+for i in range(row+1):
+    for j in range(i):
+        print(i,end=" ")
+    print("")
+
+    
