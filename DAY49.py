9a20e1d761bded298949791a985793e604c749a6
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2023-12-23 12:04:44 +0530

    Add files via upload

---
+++ DAY49.py
@@ -0,0 +1,19 @@
+#Hii all This is DAY49(23-12-2023) Python Program to Python User Input
+# using input() to take user input
+num = input('Enter a number: ')
+
+print('You Entered:', num)
+
+print('Data type of num:', type(num))
+
+
+## Python Program to calculate the square root
+
+# Note: change this value for a different result
+
+
+# To take the input from the user
+num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
