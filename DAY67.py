a0ec65af5d8c98e42d2ede5c8e84916e048cedab
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2024-01-10 21:50:29 +0530

    Add files via upload

---
+++ DAY67.py
@@ -0,0 +1,20 @@
+#Hii all this is DAY67(10-01-2024) Today's topic is pattern
+rows = 5
+num = rows
+# reverse for loop
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print(num, end=' ')
+    print("\r")
+
+'''
+Experted Output:
+5 5 5 5 5 
+5 5 5 5 
+5 5 5 
+5 5 
+5 
+
+
+
+'''
