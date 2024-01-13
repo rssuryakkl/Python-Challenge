4b1c773edd27895537660a6df9332a3234554034
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2024-01-13 15:08:47 +0530

    Add files via upload

---
+++ DAY70.py
@@ -0,0 +1,7 @@
+#Hii all this is DAY70(13-01-2024) Today's Topic is Write a python program to find the length of a string.
+
+ss=input("Enter a string:") #user input
+lent=0 #length of count
+for _ in ss:
+    lent+=1
+print("Length of a string is :",lent)
