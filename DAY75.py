f10da206147b303f225f633c288ecb268d447792
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2024-01-18 16:09:15 +0530

    Add files via upload

---
+++ DAY75.py
@@ -0,0 +1,9 @@
+#Hii all this is DAY75(18-01-2024) Today's toipcs is Python Program to Display the multiplication Table
+
+# Multiplication table (from 1 to 10) in Python
+# To take input from the user
+num = int(input("Display multiplication table of? "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
