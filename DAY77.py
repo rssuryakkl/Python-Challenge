67279603d7bed5fefb4c5e8a09094d10d3d62958
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2024-01-20 18:44:24 +0530

    Add files via upload

---
+++ DAY77.py
@@ -0,0 +1,9 @@
+#Hii all This is DAY77(20-01-2024). Write a program that finds the sum of all the numbers in a Tuples using while loop
+
+tup=(900,99)
+s=0
+i=0
+while (i<len(tup)):
+    s=s+tup[i]
+    i+=1
+print("The sum of all numbers is :",s)
