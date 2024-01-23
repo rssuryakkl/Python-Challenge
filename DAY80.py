b61715d9caac3f6180f3cba4780caaa341911793
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2024-01-23 18:52:43 +0530

    Add files via upload

---
+++ DAY80.py
@@ -0,0 +1,13 @@
+#Hii all This is DAY80(23-01-2024) Today's Topic is Write a program to insert a value in a list at the specified location.
+#Almost 80%completed of basics of python thank's for all responces
+
+
+#user input
+li=['hi','hello','rs']
+giv=input("Enter a element to be added:")
+kk=li.insert(2,giv)
+print(kk)
+print(li)
+
+
+#This is intervew questioned for depeartment of Aeronatic Engi......
