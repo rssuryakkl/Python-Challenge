a8700857d211f585e1e8e989a92df480512d91c1
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2024-01-26 18:00:03 +0530

    Add files via upload

---
+++ DAY83.py
@@ -0,0 +1,10 @@
+#Hii All This is DAY83(26-01-2024) Today's Topic is Write a program that prints the maximum and minimum value in a dictionary
+
+marks={"m1":78 , "m2":99 , "m3":64 , "m4":999 , "m5":0}
+ 
+v = marks.values()
+maxi = max(v)
+mini = min(v)
+ 
+print("Maximum Value in Dict:",maxi)
+print("Minimum Value in Dict:",mini)
