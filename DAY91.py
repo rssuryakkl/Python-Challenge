7f5a91a268042d459b113f72e6b888e8ac7854d0
SURYA PRAKASH R <148074087+rssuryakkl@users.noreply.github.com>
2024-02-03 15:14:48 +0530

    Add files via upload

---
+++ DAY91.py
@@ -0,0 +1,30 @@
+#Hii all this is DAY91(03-02-2024)Today's  topics is Problems
+#Write a program to create a mirror image of the given string. For example, “wel” = “lew“.
+
+give=input("Enter a String:")
+print(give[::-1])
+
+
+#Write a program to swap two strings
+
+def swap(a,b):
+    temp=a
+    a=b
+    b=temp
+    print("The Swap of A:",a)
+    print("The Swap of B:",b)
+
+a=input("Enter a String for A:")
+b=input("Enter a String for B:")
+
+swap(a,b)
+
+
+
+
+
+
+
+
+
+
